ba3aae62ce302bfe93f60878caa17eb33ff924c5
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-09-26 07:43:09 +0200

    Removed unused assets. Added more opponents.

--- game/opponents.lua
+++ game/opponents.lua
@@ -1,4 +1,12 @@
-return {
+local M = {}
+
+M.TUTORIAL = { "tutorial" }
+
+M.QUICKBATTLE = { "borg", "threeeye", "grunt" }
+
+M.TOURNAMENT = {}
+
+M.ALL = {
 	tutorial = {
 		name = "The Trainer",
 		portrait = hash("wazoo"),
@@ -9,14 +17,37 @@ return {
 		emotes_won = { "Aww, that was close! Better luck next time. Come back if you want to practice some more." },
 		emotes_lost = { "Well done! Now you know everything there is to know about Rock, Paper and Scissors." },
 	},
-	arnie = {
-		name = "The Governator",
-		portrait = hash("wazoo"),
+	borg = {
+		name = "Cy the Borg",
+		portrait = hash("borg"),
 		interval = 1.25,
 		speed = 6,
 		health = 8,
 		pattern = "RPS",
-		emotes_won = { "Hasta La Vista, Baby!", "You hit like a vegetarian!", "If it bleeds, we can kill it" },
-		emotes_lost = { "I'll be back!", "What the fuck did I do wrong?!" },
+		emotes_won = { "If it bleeds, we can kill it", "Take that you sack of meat!" },
+		emotes_lost = { "Syntax error!", "Does not compute!" },
+	},
+	threeeye = {
+		name = "Threeeye",
+		portrait = hash("threeeye"),
+		interval = 1.25,
+		speed = 6,
+		health = 8,
+		pattern = "RPS",
+		emotes_won = { "Take that you filthy Terran!" },
+		emotes_lost = { "Curse you and your dirty socks!" },
+	},
+	grunt = {
+		name = "Gorkan the Grunt",
+		portrait = hash("grunt"),
+		interval = 1.25,
+		speed = 6,
+		health = 8,
+		pattern = "RPS",
+		emotes_won = { "You hit like a vegetarian!" },
+		emotes_lost = { "Whaaaaargh! That hurt!" },
 	},
 }
+
+
+return M
